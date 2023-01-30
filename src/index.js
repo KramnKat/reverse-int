5a3fb50d2ce7f0176d30c1708c3b9fda621e3ee6
Guseva Ekaterina <kramnkaterinka@gmail.com>
2023-01-30 14:45:01 +0300

    feat: realize the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+   let string = String(Math.abs(n));
+   let result = ''
+   for (let i = 0; i < string.length; i++) {
+      result = `${string[i]}${result}`
+   }
+
+   return +result;
 }
